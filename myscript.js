22ec53253e20848a885e20b7a2f376286d949ed5
Alessio-Tomei <alessiotomei22@gmail.com>
2021-12-03 17:29:29 +0100

    Cosngena Finita

---
+++ myscript.js
@@ -0,0 +1,14 @@
+let userInputNumberKm = prompt('Inserisci il numero di chilometri che vuoi percorrere. km: ');
+let userInputAge = prompt('Inserisci la tua età. Età: ');
+
+let ticketPrice = userInputNumberKm * 0.21;
+
+if (userInputAge < 18) {
+    ticketPrice -= ticketPrice * 0.2;
+}
+if (userInputAge >= 65) {
+    ticketPrice -= ticketPrice * 0.4;
+}
+
+ticketPrice = parseFloat(ticketPrice.toFixed(2));
+document.writeln('Il prezzo del biglietto è: ' + ticketPrice + '€');
