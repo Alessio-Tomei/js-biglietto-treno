1b5f979291ffdc35afe6db24c23361954f0a0a81
Alessio-Tomei <alessiotomei22@gmail.com>
2021-12-09 12:33:55 +0100

    Corretto valori sconto invertiti

--- myscript.js
+++ myscript.js
@@ -4,9 +4,9 @@ let userInputAge = prompt('Inserisci la tua età. Età: ');
 let ticketPrice = userInputNumberKm * 0.21;
 
 if (userInputAge >= 65) {
-    ticketPrice -= ticketPrice * 0.2;
-} else if (userInputAge < 18) {
     ticketPrice -= ticketPrice * 0.4;
+} else if (userInputAge < 18) {
+    ticketPrice -= ticketPrice * 0.2;
 }
 
 ticketPrice = parseFloat(ticketPrice.toFixed(2));
